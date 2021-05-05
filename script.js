636c331825d3c66262863fccdac576cc2a973442
ladcs <hpladcs@gmail.com>
2021-05-05 16:53:56 -0300

    requisito 2 e 3 de brinde

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-window.onload = function() {
-  document.querySelector('.color')
-}
+window.onload = function () {
+  let line = 5;
+  let colom = 5;
+  let square = document.getElementsByClassName('color');
+  let pixel = document.getElementById('pixel-board');
+  cor = {
+    0: 'black',
+    1: 'red',
+    2: 'blue',
+    3: 'yellow'
+  };
+
+  for (let key in cor) {
+    square[key].style.backgroundColor = cor[key]
+  }
+
+  function lines() {
+    for (let linha = 0; linha < line; linha += 1) {
+      colunas();      
+    }
+  }
+
+  function colunas() {
+    for (let coluna = 0; coluna < colom; coluna += 1) {
+      
+    }
+  }
+
+};
