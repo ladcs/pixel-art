6e2efb2c625137fa14d0ca67b154a943ca306c54
ladcs <hpladcs@gmail.com>
2021-05-06 15:32:39 -0300

    requisito 8, cria botao do 9

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = function () {
   let colom = 5;
   let square = document.getElementsByClassName('color');
   let pixelFather = document.getElementById('pixel-board');
-  
+
   let cor = {
     0: 'black',
     1: 'red',
@@ -18,6 +18,7 @@ window.onload = function () {
   square[0].className = 'color selected'; 
 
   lines();
+  let arrayPixel = document.querySelectorAll('.pixel')
 
   let bt1 = square[0];
   let bt2 = square[1];
@@ -29,6 +30,25 @@ window.onload = function () {
   bt3.addEventListener("click", mudaSelecter);
   bt4.addEventListener("click", mudaSelecter);
   
+  for (let indexPixel = 0; indexPixel < arrayPixel.length; indexPixel += 1) {
+    arrayPixel[indexPixel].addEventListener('click', function (event) {
+      let eventBgColor = event.target;
+      paint(eventBgColor);
+    });
+  }
+
+  function getColorSelected() {
+    let select = document.getElementsByClassName('selected')[0];
+    let bgColor = select.style.backgroundColor;
+    let test = bgColor;
+    console.log(test);
+    return test;
+  }
+
+  function paint(event) {
+      event.style.backgroundColor = getColorSelected();
+  }
+
   function mudaSelecter(bt) {
     let remov = document.querySelector('.selected');
     remov.classList.remove('selected');
@@ -48,6 +68,7 @@ window.onload = function () {
     for (let coluna = 0; coluna < colom; coluna += 1) {
       let pixelChild = document.createElement('div');
       pixelChild.className = 'pixel';
+      pixelChild.style.backgroundColor = 'white';
       pixelLine.appendChild(pixelChild);
     }
   }
